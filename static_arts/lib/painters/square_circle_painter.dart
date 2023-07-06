b5c7a1ec07f5ebaff13218ea00de52148bda1ffc
atulya-kairati <atulyakairati@gmail.com>
2023-07-06 12:38:31 +0530

    concentric circles inside square

--- static_arts/lib/painters/square_circle_painter.dart
+++ static_arts/lib/painters/square_circle_painter.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:static_arts/utils/color_gen.dart';
 import 'package:static_arts/utils/math_utils.dart';
 
 class SquareCirclePainter extends CustomPainter {
@@ -12,29 +13,48 @@ class SquareCirclePainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     final center = Offset(size.width / 2, size.height / 2);
     final squareSide = min(size.width, size.height) / 2;
-    final paint = Paint()
-      ..style = PaintingStyle.stroke
-      ..color = Colors.cyanAccent
-      ..strokeWidth = 1;
 
     // shift the origin to center of the screen
     canvas.translate(center.dx, center.dy);
 
     // create a square
     final square = Rect.fromCircle(center: Offset.zero, radius: squareSide / 2);
+    final eclipseRect = Rect.fromCenter(
+      center: Offset.zero,
+      width: square.width / 6,
+      height: square.height,
+    );
+
+    final squarePaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..color = Colors.red
+      ..strokeWidth = 2;
 
     // calculate rotation angle
     double dAngle = MathUtils.degreeToRadians(90 / noOfSquares);
-
+    double incircleRadius = squareSide / 2;
+    double incircleGap = incircleRadius / noOfSquares;
     for (var i = 0; i < noOfSquares; i++) {
-      // draw square
-      canvas.drawRect(square, paint);
+      // get color by reducing alpha every iteration
+      squarePaint.color = ColorGen.getColorFromAHSV(
+        a: 1 - (i / noOfSquares),
+        h: 100,
+        s: .82,
+        v: .96,
+      );
 
-      // rotate the canvas each time
-      canvas.rotate(dAngle);
+      // draw square
+      canvas.drawRect(square, squarePaint);
+
+      canvas.drawCircle(
+        square.center,
+        incircleRadius - (i * incircleGap),
+        squarePaint,
+      );
+
+      // rotate the canvas acw each time
+      canvas.rotate(-dAngle);
     }
-    
-    
   }
 
   @override
